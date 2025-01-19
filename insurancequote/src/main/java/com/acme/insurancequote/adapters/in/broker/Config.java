ab0eb62dae9c507a05513496344bd71d7ae827ee
Felipe Salvador <f.salvador91@gmail.com>
2025-01-19 12:36:51 -0300

    Rodando wiremock via docker-compose Configurando credenciais AWS

--- insurancequote/src/main/java/com/acme/insurancequote/adapters/in/broker/Config.java
+++ insurancequote/src/main/java/com/acme/insurancequote/adapters/in/broker/Config.java
@@ -3,23 +3,27 @@ package com.acme.insurancequote.adapters.in.broker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.net.URI;
 
 @Configuration
-public class Config
-{
+public class Config {
     @Value("${aws-sqs-localhost}")
     private String url;
 
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
 
+        var credentials = EnvironmentVariableCredentialsProvider.create();
+
         return SqsAsyncClient.builder()
+                .credentialsProvider(credentials)
                 .endpointOverride(URI.create(url))
                 .region(Region.US_EAST_1)
                 .build();
     }
 }
+
