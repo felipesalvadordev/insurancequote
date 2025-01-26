1701af56f4635ee81d0dd9e930fca9255bf1f5fd
Felipe Salvador <f.salvador91@gmail.com>
2025-01-26 18:33:48 -0300

    Configurando credenciais AWS

--- insurancequote/src/main/java/com/acme/insurancequote/adapters/in/broker/Config.java
+++ insurancequote/src/main/java/com/acme/insurancequote/adapters/in/broker/Config.java
@@ -3,24 +3,30 @@ package com.acme.insurancequote.adapters.in.broker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.net.URI;
 
+@Profile("!(test)")
 @Configuration
 public class Config {
     @Value("${aws-sqs-localhost}")
     private String url;
 
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
-
-        var credentials = EnvironmentVariableCredentialsProvider.create();
-
         return SqsAsyncClient.builder()
-                .credentialsProvider(credentials)
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .endpointOverride(URI.create(url))
                 .region(Region.US_EAST_1)
                 .build();
